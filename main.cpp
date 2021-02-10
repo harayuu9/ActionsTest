21aab1f62d5e065fb429fc43b23b858def0ec549
harayuu9 <roudo2233@gmail.com>
2021-02-10 19:43:51 +0900

    no message

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <benchmark/benchmark.h>
+
 // TESTマクロを使う場合
 
 // 第1引数がテストケース名、第2引数がテスト名
@@ -11,3 +13,19 @@ TEST(MyLibraryTest, Class1Test) {
     ASSERT_TRUE(true)/* << u"失敗してみた２"*/;
     //...
 }
+
+
+static void CppLinqFirstOrDefault(benchmark::State& state)
+{
+
+    while (state.KeepRunning())
+    {
+        auto cnt = 0;
+        for(auto i=0;i<10000;i++)
+        {
+            cnt += i;
+        }
+    }
+}
+
+BENCHMARK(CppLinqFirstOrDefault);
